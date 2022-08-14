e023569771452be6b6f0561187edb73cd2079375
Sarath Sajan <sarathsajan1998@gmail.com>
2022-08-14 15:14:08 +0530

    get nes and basic analysis

--- codebase/app.py
+++ codebase/app.py
@@ -1,22 +1,22 @@
-from newsapi import NewsApiClient
 from env_vars.env_vars import ENV_VARS
-import time
+from newsapi import NewsApiClient
+from textblob import TextBlob
+# import time
 
-# Init
 ENVIRONMENT_VARIABLES = ENV_VARS()
 newsapi = NewsApiClient(api_key=ENVIRONMENT_VARIABLES["NEWS_API"])
 
 # /v2/top-headlines
-top_headlines = newsapi.get_top_headlines(q='India', language='en', country='in')
-print (top_headlines)
-time.sleep(10)
-
-# # /v2/everything
-# all_articles = newsapi.get_everything(q='bitcoin', sources='bbc-news,the-verge', domains='bbc.co.uk,techcrunch.com', from_param='2022-08-01', to='2022-08-12', language='en', sort_by='relevancy', page=2)
-# print (all_articles)
-# time.sleep(10)
-
-# # /v2/top-headlines/sources
-# sources = newsapi.get_sources()
-# print (sources)
-# time.sleep(10)
+top_headlines = newsapi.get_top_headlines(language='en', sources='reuters,associated-press')
+print('Status\t\t:\t', top_headlines['status'])
+print('Result count\t:\t', top_headlines['totalResults'])
+for article in top_headlines['articles']:
+    print('------------------------------------------------------------------')
+    print('Source\t\t:\t', article['source']['name'])
+    print('Description\t:\t', article['title'])
+    description = article['title']
+    blob = TextBlob(description)
+    # print(blob.tags)
+    print(blob.noun_phrases)
+    for sentence in blob.sentences:
+        print(sentence.sentiment)
